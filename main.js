a6ab78d4270573ef27056c6a293d242a8f5d0761
arteck <6681528+arteck@users.noreply.github.com>
2025-07-28 14:46:32 +0200

    "mode": "daemon"

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const path = require('path');
 
+
 global.fetch = (...args) => import('node-fetch').then(mod => mod.default(...args));
 
 
@@ -24,54 +25,117 @@ class xsenseControll  extends utils.Adapter {
         this.json2iob = new Json2iobXSense(this);
 
         this.on('ready', this.onReady.bind(this));
+        this.on('schedule', this.onSchedule.bind(this));
         this.on('unload', this.onUnload.bind(this));
     }
 
     async onReady() {
         try {
-            this.log.info('Starte Login bei X-Sense...');
+            this.log.info('Start X-Sense...');
 
-            const response = await this.xsenseLogin();
+            this.python = await this.setupXSenseEnvironment();
 
-            let knownDevices = [];
+            if (!this.python) {
+                this.log.error('Python environment could not be initialized.');
+                return;
+            }
 
-            let iobDevices = await this.getDevicesAsync();
-
-            const parsed = tools.parseXSenseOutput(response, knownDevices );
-
-            await this.json2iob.parse('xsense.0', parsed, { forceIndex: true, write: true });
-
+            await this.datenVerarbeiten();
             this.setState('info.connection', true, true);
-
-
 
         } catch (err) {
             this.setState('info.connection', false, true);
-            this.log.error(`Fehler beim Login oder Setup: ${err.message}`);
+            this.log.error(`Error on Login or Setup: ${err.message}`);
             return;
         }
     }
 
-
-    async xsenseLogin() {
-        try {
-            const scriptPath = path.join(tools.getDataFolder(this), 'run_xsense.py');
-            const cmd = `python3 ${scriptPath} ${this.config.userEmail} ${this.config.userPassword}`;
-
-            const { stdout, stderr } = await exec(cmd);
-
-            if (stderr) {
-                this.log.warn('Warnung beim Ausführen des Scripts: ' + stderr);
+    async onSchedule() {
+        if (!this.python) {
+            this.log.warn('Python environment not initialized. Trying again...');
+            this.python = await this.setupXSenseEnvironment();
+            if (!this.python) {
+                this.setState('info.connection', false, true);
+                return;
             }
+        }
 
-            return stdout;
+        await this.datenVerarbeiten();
 
-        } catch (error) {
-            this.log.error('Fehler beim Ausführen des Scripts: ' + error.message);
+    }
+
+    async datenVerarbeiten() {
+        const response = await this.callBridge(this.python, this.config.userEmail, this.config.userPassword);
+
+        if (response) {
+            let knownDevices = [];
+            let iobDevices = await this.getDevicesAsync();
+
+            const parsed = tools.parseXSenseOutput(response, knownDevices);
+
+            await this.json2iob.parse('xsense.0', parsed, {forceIndex: true, write: true});
+
+        }
+    }
+
+
+    async setupXSenseEnvironment() {
+        try {
+            const { getVenv } = await import('autopy');
+            const pfadPythonScript = tools.getDataFolder(this);
+            this.log.debug('[XSense] getVenv imported from autopy');
+
+            const python = await getVenv({
+                name: 'xsense-env',
+                pythonVersion: '~3.13',
+                requirements: [
+                    { name: 'requests', version: '' },
+                    { name: 'aiohttp', version: '' }
+                ],
+                extraPackages: [
+                    pfadPythonScript + 'python-xsense'
+                ]
+            });
+
+            this.log.debug('[XSense] Python environment ready at ' + python.path);
+
+            return python;
+        } catch (err) {
+            this.log.error('[XSense] Error on create ' + err.message);
+            this.log.debug(err.stack);
             return null;
         }
     }
 
+    async callBridge(python, email, password) {
+        return new Promise((resolve, reject) => {
+            const scriptPath = path.join(__dirname, 'python', 'run_xsense.py');
+            const proc = python('python3', [scriptPath, email, password]);
+
+            let result = '';
+
+            proc.stdout?.on('data', data => {
+                result += data.toString();
+            });
+
+            proc.stderr?.on('data', data => {
+                this.log.warn('callBridge warning: ' + data.toString());
+            });
+
+            proc.on('error', err => {
+                reject(err);
+            });
+
+            proc.on('close', code => {
+                this.log.info('callBridge script exited with code ' + code);
+                if (code === 0) {
+                    resolve(result.trim());
+                } else {
+                    reject(new Error(`callBridge Python script exited with code ${code}`));
+                }
+            });
+        });
+    }
 
 
     async onUnload(callback) {
---
+++ scripts/setup-xsense.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const { getVenv } = require('autopy');
+
+(async () => {
+  const envPath = path.join(__dirname, '..', '.autopy', 'xsense-env');
+
+  if (fs.existsSync(envPath)) {
+    console.log('Python-Umgebung "xsense-env" ist bereits vorhanden. Setup wird übersprungen.');
+    return;
+  }
+
+  try {
+    const python = await getVenv({
+      name: 'xsense-env',
+      pythonVersion: '~3.11',
+      requirements: [
+        { name: 'requests' },
+        { name: 'aiohttp' }
+      ],
+      extraPackages: ['git+https://github.com/theosnel/python-xsense.git']
+    });
+
+    console.log('XSense-Umgebung erfolgreich eingerichtet.');
+  } catch (err) {
+    console.error('Fehler beim Einrichten der Umgebung:', err);
+  }
+})();
