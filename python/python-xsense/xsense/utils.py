531318d145d8c6ebfa1772abdf4b11b51b7869ac
arteck <6681528+arteck@users.noreply.github.com>
2025-07-29 15:20:24 +0200

    Update utils.py

--- python/python-xsense/xsense/utils.py
+++ python/python-xsense/xsense/utils.py
@@ -1,5 +1,6 @@
 import argparse
 import contextlib
+import json
 
 from xsense.base import XSenseBase
 
@@ -37,7 +38,42 @@ def dump_environment(env: XSenseBase):
             print(f'# {s.name} ({s_id})')
             for d_id, d in s.devices.items():
                 dump_device(d)
+                
+def environment_to_dict(env: XSenseBase):
+    result = {}
+    for h_id, h in env.houses.items():
+        house = {
+            'name': h.name,
+            'id': h_id,
+            'stations': {}
+        }
+        for s_id, s in h.stations.items():
+            station = {
+                'name': s.name,
+                'id': s_id,
+                'serial': s.sn,
+                'type': s.type,
+                'online': s.online,
+                'values': s.data,
+                'devices': {}
+            }
+            for d_id, d in s.devices.items():
+                device = {
+                    'name': d.name,
+                    'id': d_id,
+                    'serial': d.sn,
+                    'type': d.type,
+                    'online': d.online,
+                    'values': d.data
+                }
+                station['devices'][d_id] = device
+            house['stations'][s_id] = station
+        result[h_id] = house
+    return result
 
+def dump_environment_json(env: XSenseBase):
+    env_dict = environment_to_dict(env)
+    print(json.dumps(env_dict, indent=2, ensure_ascii=False))
 
 def dump_device(d):
     print(f'{d.name} ({d.type}):')
